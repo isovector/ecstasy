ca06a9d74908fa0122b622543e835db83e3e929f
Sandy Maguire <sandy@sandymaguire.me>
2018-07-06 13:00:19 +0300

    generic channel

--- src/Control/Monad/Accursed.hs
+++ src/Control/Monad/Accursed.hs
@@ -1,17 +1,26 @@
-{-# LANGUAGE BangPatterns        #-}
-{-# LANGUAGE DeriveFunctor       #-}
-{-# LANGUAGE FlexibleContexts    #-}
-{-# LANGUAGE RankNTypes          #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE BangPatterns          #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveFunctor         #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RankNTypes            #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE UndecidableInstances  #-}
 
 module Control.Monad.Accursed where
 
 import           Control.Applicative (Alternative (..))
 import           Control.Exception (Exception, throw, catch)
+import           Control.Monad.Codensity (improve, lowerCodensity)
 import qualified Control.Monad.Free as F
-import Control.Monad.Codensity (improve, lowerCodensity)
 import           Control.Monad.Trans.Maybe (runMaybeT)
-import           Control.Monad.Writer.Strict (runWriter, tell, guard)
+import           Control.Monad.Writer.Strict (runWriter, tell)
+import           GHC.Generics
+import           GHC.TypeLits
 import           System.IO.Unsafe (unsafePerformIO)
 
 
@@ -20,12 +29,12 @@ import           System.IO.Unsafe (unsafePerformIO)
 -- | The 'Accursed' monad in which evaluation of 'curse' will be interpreted as
 -- 'empty' at the time it happens. Under very specific circumstances, this
 -- allows some degree of static analysis over free monads. The rest of the time
--- it will lead to terrible, unutterable bugs.
+-- it will lead to terror, madness and runtime crashes.
 data Accursed f a
   = Pure a
   | Free (f (Accursed f a))
   | Empty
-
+  deriving (Generic, Generic1)
 
 instance Functor f => Functor (Accursed f) where
   fmap f (Pure a)  = unsafePerformIO $
@@ -36,7 +45,6 @@ instance Functor f => Functor (Accursed f) where
   fmap f (Free fa) = Free (fmap f <$> fa)
   fmap _ Empty     = Empty
   {-# INLINE fmap #-}
-
 
 instance Functor f => Applicative (Accursed f) where
   pure = Pure
@@ -63,7 +71,6 @@ instance Functor f => Applicative (Accursed f) where
   _ <*> Empty = Empty
   {-# INLINE (<*>) #-}
 
-
 instance Functor f => Monad (Accursed f) where
   return = pure
   {-# INLINE return #-}
@@ -76,16 +83,31 @@ instance Functor f => Monad (Accursed f) where
   Empty >>= _ = Empty
   {-# INLINE (>>=) #-}
 
-
 instance Functor f => Alternative (Accursed f) where
   empty = Empty
   Empty <|> a = a
   a     <|> _ = a
 
 
+------------------------------------------------------------------------------
+-- | Lift a natural transformation from 'f' to 'g' over 'Accursed'.
+hoistAccursed
+    :: Functor g
+    => (forall x. f x -> g x)
+    -> Accursed f a
+    -> Accursed g a
+hoistAccursed _ (Pure a)   = Pure a
+hoistAccursed nat (Free f) = Free $ fmap (hoistAccursed nat) $ nat f
+hoistAccursed _ Empty      = Empty
+
+
+------------------------------------------------------------------------------
+-- | Perform a best-effort analysis of a free monad.
 analyze
-    :: (Functor f)
-    => (forall b. f (Accursed f b) -> (forall z. z) -> Accursed f b)
+    :: Functor f
+    => (forall b. f (Accursed f b) -> Accursed f b)
+       -- ^ The following function. Consider using 'channel' to get an
+       -- automatic implementation for this.
     -> Accursed f a
     -> [f ()]
 analyze c = snd . runWriter . runMaybeT . go
@@ -100,7 +122,7 @@ analyze c = snd . runWriter . runMaybeT . go
       tell . pure $ () <$ f
       unsafePerformIO $
         catch
-          ( let !z = c f curse
+          ( let !z = c f
              in pure $ go z)
           (\(_ :: Curse) -> pure $ empty)
     {-# INLINE go #-}
@@ -121,52 +143,103 @@ curse :: a
 curse = throw Curse
 
 
-accurse
+------------------------------------------------------------------------------
+-- | Convert a 'F.Free' monad into an 'Accursed' monad.
+corrupt
     :: Functor f
     => F.Free f a
     -> Accursed f a
-accurse (F.Pure a) = Pure a
-accurse (F.Free f) = Free $ fmap accurse f
-
-sanctify
-    :: (Functor f, Alternative f)
-    => Accursed f a
-    -> F.Free f a
-sanctify (Pure a) = F.Pure a
-sanctify (Free f) = F.Free $ fmap sanctify f
-sanctify Empty    = empty
+corrupt (F.Pure a) = Pure a
+corrupt (F.Free f) = Free $ fmap corrupt f
 
 
--- cont :: Accursed Pattern Bool
--- cont = Free . fmap pure $ Cont id
+------------------------------------------------------------------------------
+-- | Helper class to derive 'channel' generically.
+class GChannel p f where
+  gchannel :: f (Accursed p b) -> Accursed p b
 
-cont :: F.MonadFree Pattern m => m Bool
-cont = F.liftF $ Cont id
+instance TypeError
+    ( Text "Missing continuation parameter when attempting to derive 'channel'"
+ :$$: Text "Expected a type variable, but got "
+ :<>: ShowType a)
+      => GChannel p (K1 _1 a) where
+  gchannel = undefined
+  {-# INLINE gchannel #-}
+
+instance {-# OVERLAPPING #-} TypeError
+    ( Text "Missing continuation parameter when attempting to derive 'channel'"
+ :$$: Text "Expected a type variable, but the constructor '"
+ :<>: Text tyConName
+ :<>: Text "' has none")
+      => GChannel p (C1 ('MetaCons tyConName _b _c) U1) where
+  gchannel = undefined
+  {-# INLINE gchannel #-}
+
+instance GChannel p V1 where
+  gchannel _ = undefined
+  {-# INLINE gchannel #-}
+
+instance GChannel p (Rec1 ((->) a)) where
+  gchannel (Rec1 z) = z curse
+  {-# INLINE gchannel #-}
+
+instance GChannel p Par1 where
+  gchannel (Par1 z) = z
+  {-# INLINE gchannel #-}
+
+instance GChannel p g => GChannel p (f :*: g) where
+  gchannel (_ :*: b) = gchannel b
+  {-# INLINE gchannel #-}
+
+instance (GChannel p f, GChannel p g) => GChannel p (f :+: g) where
+  gchannel (L1 f) = gchannel f
+  gchannel (R1 g) = gchannel g
+  {-# INLINE gchannel #-}
+
+instance GChannel p f => GChannel p (M1 _1 _2 f) where
+  gchannel (M1 f) = gchannel f
+  {-# INLINE gchannel #-}
 
 
-spook
-    :: (Functor f, Alternative f)
-    => (forall m. (F.MonadFree f m, Alternative m) => m a)
+------------------------------------------------------------------------------
+-- | Generically derived continuation follower; intended to be used as the
+-- first parameter for 'analyze'.
+channel
+    :: (Generic1 f, GChannel f (Rep1 f))
+    => f (Accursed f a)
     -> Accursed f a
-spook f = accurse $ lowerCodensity f
-
-
-zoom :: [Pattern ()]
-zoom = analyze zoo $ accurse $ improve $ do
-  x <- cont
-  y <- cont
-  if x
-     then pure True
-     else cont
-  where
-    zoo (Cont !k) z = k z
+channel = gchannel . from1
+{-# INLINE channel #-}
 
 
 
-data Pattern a
-  = Cont (Bool -> a)
-  deriving (Functor)
+-- cont :: F.MonadFree Pattern m => m Bool
+-- cont = F.liftF $ Cont id
 
-instance Show (Pattern a) where
-  show (Cont _) = "Cont"
+-- action :: F.MonadFree Pattern m => Int -> m ()
+-- action i = F.liftF $ Action i ()
+
+
+-- spook
+--     :: (Functor f, Alternative f)
+--     => (forall m. (F.MonadFree f m, Alternative m) => m a)
+--     -> Accursed f a
+-- spook f = corrupt $ lowerCodensity f
+
+
+-- zoom :: [Pattern ()]
+-- zoom = analyze channel . corrupt $ improve $ do
+--   x <- cont
+--   action 5
+--   y <- cont
+--   if x
+--      then pure True
+--      else cont
+
+
+
+-- data Pattern a
+--   = Cont (Bool -> a)
+--   | Action Int a
+--   deriving (Functor, Generic, Generic1)
 
